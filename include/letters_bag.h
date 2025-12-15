b0045391f4fb5d864bd8db7d7b31a68c2c41e81f
ikeer023 <ikerresinouni@gmail.com>
2025-12-15 21:01:25 +0000

    letters_bag.h

--- include/letters_bag.h
+++ include/letters_bag.h
@@ -3,15 +3,27 @@
 
 #include "letters_set.h"
 #include <vector>
-#include <stdlib.h>
-#include <time.h>
-
+#include <stdlib.h> 
+#include <time.h>  
+ 
 using namespace std;
-
 
 class LettersBag {
 private:
- //A completar
+    vector<char> letters;  // Contenedor de letras
+
+public:
+    LettersBag();  // Constructor vacío
+    LettersBag(const LettersSet &letterSet);  // Constructor con LettersSet
+
+    char extractLetter();  // Extrae una letra
+    vector<char> extractLetter(int num);  // Extrae varias letras
+
+    int size() const;  // Devuelve el tamaño de la bolsa
+    bool empty() const;  // Indica si la bolsa está vacía
+    void clear();  // Limpia la bolsa de letras
 };
 
 #endif
+
+
