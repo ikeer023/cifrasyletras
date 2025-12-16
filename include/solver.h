5b8e606cddce937012c5c9dfaa6d56db7c10098e
ikeer023 <ikerresinouni@gmail.com>
2025-12-16 17:49:52 +0000

    solver.h

--- include/solver.h
+++ include/solver.h
@@ -2,20 +2,28 @@
 #define __SOLVER_H__
 
 #include <string>
-#include <utility>
 #include <vector>
+#include <map>
 
 #include "letters_set.h"
 #include "dictionary.h"
 
 using namespace std;
 
-class Solver{
+class Solver {
 private:
-  Dictionary dictionary;
-  LettersSet letters_set;
+    Dictionary dictionary;
+    LettersSet letters_set;
 
-  //A completar
+    // Comprueba si una palabra puede construirse con las letras que tenemos disponibles
+    bool canBuildWord(const string &palabra, const vector<char> &letras_disponibles) const;
+
+public:
+    // Constructor
+    Solver(const Dictionary &dic, const LettersSet &letters);
+
+    // Obtiene las mejores soluciones (por longitud o puntuación)
+    vector<string> getSolutions(const vector<char> &letras_disponibles, bool modo_puntuacion);
 };
 
 #endif // __SOLVER_H__
